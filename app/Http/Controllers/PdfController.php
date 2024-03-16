a6ad9a8e80927bd52df6f6e03ffa93e4bae5d9cf
Faisal Mulya Santosa - 3RT8D <3rt8d@mail.com>
2024-03-16 21:55:38 +0700

    first commit

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+class PdfController extends Controller
+{
+    public function generatePdf(){
+        $data = ['title' => 'domPDF in Laravel 10'];
+        $pdf = PDF::loadView('pdf.document', $data);
+        return $pdf->download('document.pdf');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <?php use Barryvdh\DomPDF\Facade\Pdf;?>
+        <h1>hello word</h1>
+
+        <table border="2">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Alamat</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Agung</td>
+                    <td>Gg.wp</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <a href="{{ route('pdf.generate') }}">Download PDF</a>
+    </body>
+</html>
